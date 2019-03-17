1ec16b8631317d86a6f8afc963e8f0c4a4d5b3a1
Andrey Kandyba <andreikandyba@gmail.com>
2019-03-17 18:00:04 +0200

    final app

--- bingebot/ViewController.swift
+++ bingebot/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var showLabel: UILabel!
+    @IBOutlet weak var randomShowLabel: UILabel!
+    @IBOutlet weak var bingebotSpokenLabel: UILabel!
+    @IBOutlet weak var addShowTextField: UITextField!
+    @IBOutlet weak var addShowBtn: UIButton!
+    @IBOutlet weak var randomShowStackView: UIStackView!
+    @IBOutlet weak var addShowStackView: UIStackView!
+    @IBOutlet weak var showStackView: UIStackView!
+    
+    var show: [String] = []
+    
+    func updateShowLabel() {
+        showLabel.text = show.joined(separator: ", ")
+    }
+    
+    @IBAction func randomBingeShowBtnWasPressed(_ sender: Any) {
+        randomShowLabel.text = show.randomElement()
+        randomShowLabel.isHidden = false
+        bingebotSpokenLabel.isHidden = false
+    }
+    
+    @IBAction func addShowBtnWasPressed(_ sender: Any) {
+        guard let showName = addShowTextField.text else {return}
+        show.append(showName)
+        updateShowLabel()
+        addShowTextField.text = ""
+        showStackView.isHidden = false
+        if show.count > 1 {
+            randomShowStackView.isHidden = false
+            bingebotSpokenLabel.isHidden = true
+            randomShowLabel.isHidden = true
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        showStackView.isHidden = true
+        randomShowStackView.isHidden = true
     }
-
-
+    
+    
 }
 
