ae7fa84aa307af83908aa5e6bb50ec71d918c1bb
Andrey Kandyba <andreikandyba@gmail.com>
2019-03-17 14:29:08 +0200

    Initial Commit

---
+++ bingebot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bingebot
+//
+//  Created by Andrey Kandyba on 17/03/2019.
+//  Copyright © 2019 Andrey Kandyba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
